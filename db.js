07ee4b30a201ba4b3f66f79fa388f557cdaca612
priyanshusharman <priyanshuk1900@gmaul.com>
2023-11-03 10:29:32 +0530

    incress timeout to connect

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 const dburl = 'mongodb+srv://aidpriyanshu:uQV9tbTDN9uuPlhV@webnote.s52wq4d.mongodb.net/';
 
+// const coonectToMongoose = () => {
+//     mongoose.connect(dburl)
+//         .then(console.log("Connected"))
+// }
 const coonectToMongoose = () => {
-    mongoose.connect(dburl)
-        .then(console.log("Connected"))
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // Set to a higher value if needed
+  });
 }
+
 module.exports = coonectToMongoose;
